7b14c5ed8c05d09b85c136d36191025c63b8c742
nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2026-03-16 17:25:29 -0300

    init commit and some work

---
+++ Assets/Scripts/PlanetsMotion.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlanetsMotion : MonoBehaviour
+{
+    [SerializeField] private float semiMajorAxis;
+    [SerializeField] private float semiMinorAxis;
+    [SerializeField] private float period;
+    [SerializeField] private float rotationAngle;
+
+    private float _meanMotion;
+    private float _eccentricity;
+    private float _timeFromStart;
+
+    private void OnEnable()
+    {
+        _eccentricity = Mathf.Sqrt(1.0f - semiMinorAxis * semiMinorAxis / (semiMajorAxis * semiMajorAxis));
+    }
+
+    private void Update()
+    {
+        _meanMotion = 2 * Mathf.PI / period;
+        //TranslateKeplers();
+        transform.Rotate(Vector3.up, rotationAngle * Time.deltaTime);
+        _timeFromStart += Time.deltaTime;
+    }
+    
+    private void TranslateKeplers()
+    {
+        // does not work 
+        var meanAnomaly = _meanMotion * _timeFromStart;
+
+        //since sin(E) = y/b 
+        /*
+         From kepler's eq:
+         M = E - e sin(E)
+         M + e sin(E) = E
+         M + e (y/b) = E
+         */
+        var eccentricAnomaly = meanAnomaly + _eccentricity *
+            (transform.position.z / semiMinorAxis);
+        var x = semiMajorAxis * (Mathf.Cos(eccentricAnomaly) - _eccentricity);
+        var z = semiMinorAxis * Mathf.Sin(eccentricAnomaly);
+
+        transform.Translate(new Vector3(x, 0, z));
+    }
+}
