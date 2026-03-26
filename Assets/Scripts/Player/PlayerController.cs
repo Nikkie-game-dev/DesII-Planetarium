1b152556fde20614178558168bdc63ce7450ec11
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2026-03-26 13:45:01 -0300

    add physics to player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,25 +1,55 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Player
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private InputActionReference move;
-        [SerializeField] private float speed;
+        [SerializeField] private InputActionReference toggleDamping;
+        [SerializeField] private float engineForce;
+        [SerializeField] private float damping;
+
+        [CanBeNull] private Rigidbody _rigidbody;
         private Vector2 _movement;
         private bool _isMoving;
 
         private void Awake()
         {
-            if (move == null)  return; // why unity does not update to c# 14??
-            move.action.performed += Move;
-            move.action.canceled += Stop;
+            _rigidbody = GetComponent<Rigidbody>();
+            if (_rigidbody != null) _rigidbody.linearDamping = damping;
+
+            if (move != null) 
+            {
+                move.action.performed += Move;
+                move.action.canceled += Stop;
+            }
+
+            if (toggleDamping != null)
+            {
+                toggleDamping.action.performed += ToggleDamping;
+            }
+        }
+
+        private void ToggleDamping(InputAction.CallbackContext _)
+        {
+            if (_rigidbody == null) return;
+
+            if (_rigidbody.linearDamping > float.Epsilon)
+            {
+                _rigidbody.linearDamping = 0;
+            }
+            else
+            {
+                _rigidbody.linearDamping = damping;
+            }
         }
 
         private void Move(InputAction.CallbackContext ctx)
         {
-            _movement = Time.deltaTime * speed * ctx.ReadValue<Vector2>();
+            _movement = Time.deltaTime * engineForce * ctx.ReadValue<Vector2>();
             _isMoving = true;
         }
 
@@ -27,8 +57,7 @@ namespace Player
 
         private void Update()
         {
-            if (_isMoving) transform.Translate(_movement.x, 0, _movement.y);
-            Debug.Log(_movement);
+            if (_isMoving) _rigidbody?.AddForce(_movement.x, 0, _movement.y, ForceMode.Acceleration);
         }
 
         private void OnDestroy()
